ab0d435f845f5602872e6bb8a5b4327ca3cdae3c
thearith <sothearith.sreang@gmail.com>
2016-01-10 17:58:39 +0800

    add socket io

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log("a user connection");
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  })
 })
 
 http.listen(3000, function() {
